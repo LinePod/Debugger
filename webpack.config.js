3122c90446868bff593c8c811c0adc46ff307686
David Stangl <david.stangl@student.hpi.de>
2016-11-28 15:53:05 +0100

    Setup typescript + react project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+    },
+
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js',],
+    },
+
+    module: {
+        loaders: [
+            { test: /\.tsx?$/, loader: 'ts-loader' },
+        ],
+
+        preLoaders: [
+            { test: /\.js$/, loader: 'source-map-loader' },
+        ],
+    },
+
+    externals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+    },
+}
