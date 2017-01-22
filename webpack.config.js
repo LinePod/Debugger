d9dcb1e07f136be31e1e4e6632a1245ca6f87ca6
David Stangl <david.stangl@student.hpi.de>
2017-01-22 14:51:22 +0100

    Generate/bundle HTML and CSS with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 var path = require('path');
 var ats = require('awesome-typescript-loader');
+var htmlTemplate = require('html-webpack-template');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
 
 module.exports = {
     entry: './src/index.tsx',
@@ -16,11 +20,21 @@ module.exports = {
 
     module: {
         loaders: [
-            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+            },
+            {
+                test: /\.css/,
+                loader: ExtractTextPlugin.extract('css-loader'),
+            },
         ],
 
         preLoaders: [
-            { test: /\.js$/, loader: 'source-map-loader' },
+            {
+                test: /\.js$/,
+                loader: 'source-map-loader',
+            },
         ],
     },
 
@@ -31,5 +45,34 @@ module.exports = {
 
     plugins: [
         new ats.CheckerPlugin(),
+        new ExtractTextPlugin('styles.css'),
+        new HtmlWebpackPlugin({
+            inject: false,
+            template: htmlTemplate,
+            minify: {
+                // Remove tons of empty lines from template
+                collapseWhitespace: true,
+                preserveLineBreaks: true,
+            },
+            appMountId: 'react-root',
+            title: 'Linespace debugger',
+        }),
+        new HtmlWebpackExternalsPlugin(
+            [
+                {
+                    name: 'react',
+                    'var': 'React',
+                    path: 'react/dist/react.js',
+                },
+                {
+                    name: 'react-dom',
+                    'var': 'ReactDOM',
+                    path: 'react-dom/dist/react-dom.js',
+                },
+            ],
+            {
+                basedir: path.join(__dirname, 'node_modules'),
+            }
+        ),
     ],
 }
